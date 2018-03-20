2b001a824ea09e73fea0daf855ff89c88c7e7666
Kyle McNutt <mcnuttkyle93@gmail.com>
2018-03-20 02:28:23 -0700

    Adding Crashlytics.

--- app/src/main/java/com/example/mcnutt/inclassdemo/MainActivity.java
+++ app/src/main/java/com/example/mcnutt/inclassdemo/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         this.helloWorldText = findViewById(R.id.hello_world_text);
     }
