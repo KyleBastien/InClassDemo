8cf063eafa7f223e03ba6d360cdc69809fbd574c
Kyle McNutt <mcnuttkyle93@gmail.com>
2018-03-30 20:23:42 -0700

    One way to click a button.

--- app/src/main/java/com/example/mcnutt/inclassdemo/MainActivity.java
+++ app/src/main/java/com/example/mcnutt/inclassdemo/MainActivity.java
@@ -1,37 +1,45 @@
 package com.example.mcnutt.inclassdemo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
 
-    private TextView helloWorldText;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
-        this.helloWorldText = findViewById(R.id.hello_world_text);
+        relativeLayout = findViewById(R.id.relativeLayout);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        button1.setOnClickListener(MainActivity.this);
+        button2.setOnClickListener(MainActivity.this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.action_bye) {
-            this.helloWorldText.setText(R.string.say_bye);
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                relativeLayout.setBackgroundColor(Color.GREEN);
+                Log.i(TAG, "Change color to green");
+                break;
+            case R.id.button2:
+                relativeLayout.setBackgroundColor(Color.BLUE);
+                Log.i(TAG, "Change color to blue");
+                break;
         }
-        return super.onOptionsItemSelected(item);
     }
 }
