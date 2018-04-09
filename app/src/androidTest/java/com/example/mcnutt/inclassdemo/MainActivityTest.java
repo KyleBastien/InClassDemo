d98270db409fdcd7aa65db03e58815c49c36099f
Kyle McNutt <mcnuttkyle93@gmail.com>
2018-04-09 00:16:53 -0700

    Fixing tests by dropping focus of the EditText in MainActivity. Also added some try/catch's to take care of Intent.release possibility.

--- app/src/androidTest/java/com/example/mcnutt/inclassdemo/MainActivityTest.java
+++ app/src/androidTest/java/com/example/mcnutt/inclassdemo/MainActivityTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
@@ -44,52 +45,70 @@ public class MainActivityTest {
     public void canGoToSecondActivityWithMessage() {
         onView(withId(R.id.nameEditText)).perform(typeText("Kyle McNutt"));
 
-        Intents.init();
-        onView(withId(R.id.secondActivityBtn)).perform(click());
-        intended(hasComponent(SecondActivity.class.getName()));
-        intended(hasExtra(Constants.KEY_NAME, "Kyle McNutt"));
-        intended(hasExtra(Constants.KEY_AGE, 24));
-        Intents.release();
+        try {
+            Intents.init();
+            onView(withId(R.id.secondActivityBtn)).perform(scrollTo(), click());
+            intended(hasComponent(SecondActivity.class.getName()));
+            intended(hasExtra(Constants.KEY_NAME, "Kyle McNutt"));
+            intended(hasExtra(Constants.KEY_AGE, 24));
+        } finally {
+            Intents.release();
+        }
     }
 
     @Test
     public void canGoToWeightLayoutExample() {
-        Intents.init();
-        onView(withId(R.id.weightExampleBtn)).perform(click());
-        intended(hasComponent(WeightLayoutActivity.class.getName()));
-        Intents.release();
+        try {
+            Intents.init();
+            onView(withId(R.id.weightExampleBtn)).perform(scrollTo(), click());
+            intended(hasComponent(WeightLayoutActivity.class.getName()));
+        } finally {
+            Intents.release();
+        }
     }
 
     @Test
     public void canGoToNestedLinearLayoutExample() {
-        Intents.init();
-        onView(withId(R.id.nestedExampleBtn)).perform(click());
-        intended(hasComponent(NestedLinearLayoutActivity.class.getName()));
-        Intents.release();
+        try {
+            Intents.init();
+            onView(withId(R.id.nestedExampleBtn)).perform(scrollTo(), click());
+            intended(hasComponent(NestedLinearLayoutActivity.class.getName()));
+        } finally {
+            Intents.release();
+        }
     }
 
     @Test
     public void canGoToRelativeNestedLayoutExample() {
-        Intents.init();
-        onView(withId(R.id.relativeExampleBtn)).perform(click());
-        intended(hasComponent(RelativeLayoutNestedExample.class.getName()));
-        Intents.release();
+        try {
+            Intents.init();
+            onView(withId(R.id.relativeExampleBtn)).perform(scrollTo(), click());
+            intended(hasComponent(RelativeLayoutNestedExample.class.getName()));
+        } finally {
+            Intents.release();
+        }
     }
 
     @Test
     public void canGoToFrameLayoutExample() {
-        Intents.init();
-        onView(withId(R.id.frameLayoutExampleBtn)).perform(click());
-        intended(hasComponent(FrameLayoutPictureActivity.class.getName()));
-        Intents.release();
+        try {
+            Intents.init();
+            onView(withId(R.id.frameLayoutExampleBtn)).perform(scrollTo(), click());
+            intended(hasComponent(FrameLayoutPictureActivity.class.getName()));
+        } finally {
+            Intents.release();
+        }
     }
 
     @Test
     public void canGoToTableLayoutExample() {
-        Intents.init();
-        onView(withId(R.id.tableLayoutExample)).perform(click());
-        intended(hasComponent(TableLayoutExampleActivity.class.getName()));
-        Intents.release();
+        try {
+            Intents.init();
+            onView(withId(R.id.tableLayoutExample)).perform(scrollTo(), click());
+            intended(hasComponent(TableLayoutExampleActivity.class.getName()));
+        } finally {
+            Intents.release();
+        }
     }
 
 }
