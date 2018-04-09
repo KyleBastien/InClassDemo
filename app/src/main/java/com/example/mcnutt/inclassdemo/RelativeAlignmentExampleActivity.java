489eb382317d881488507fd8971db8c836daffec
Kyle McNutt <mcnuttkyle93@gmail.com>
2018-04-08 23:49:07 -0700

    Adding a bunch of Layout examples.

---
+++ app/src/main/java/com/example/mcnutt/inclassdemo/RelativeAlignmentExampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.mcnutt.inclassdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeAlignmentExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_alignment_example);
+    }
+}
