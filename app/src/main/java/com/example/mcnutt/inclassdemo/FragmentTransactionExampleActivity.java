b42cbd8fe8dd6b45b13e4dc05f0293ecbf2e0ea6
Kyle McNutt <mcnuttkyle93@gmail.com>
2018-04-16 20:31:47 -0700

    Adding a new Activity to do Transaction examples in

---
+++ app/src/main/java/com/example/mcnutt/inclassdemo/FragmentTransactionExampleActivity.java
@@ -0,0 +1,26 @@
+package com.example.mcnutt.inclassdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FragmentTransactionExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_transaction_example);
+    }
+
+    public void addFragmentA(View view) {
+    }
+
+    public void removeFragmentA(View view) {
+    }
+
+    public void addFragmentB(View view) {
+    }
+
+    public void removeFragmentB(View view) {
+    }
+}
