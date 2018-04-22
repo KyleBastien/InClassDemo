861826dfaca7a2f21ea16ca359597432cc96f0ff
KyleMcNutt <mcnuttkyle93@gmail.com>
2018-04-22 15:25:29 -0700

    Revert "Example of Passing Data to a Fragment via a Bundle"
    This reverts commit 479a14337445f0bdfad0ef837d6b7485b939ec62.

--- app/src/main/java/com/example/mcnutt/inclassdemo/Constants.java
+++ app/src/main/java/com/example/mcnutt/inclassdemo/Constants.java
@@ -4,6 +4,4 @@ class Constants {
     static final String KEY_NAME = "name";
     static final String KEY_BUTTON_TXT = "button_txt";
     static final String KEY_AGE = "age";
-    static final String KEY_FIRST_NUMBER = "first_number";
-    static final String KEY_SECOND_NUMBER = "second_number";
 }
--- app/src/main/java/com/example/mcnutt/inclassdemo/FragmentForData.java
+++ app/src/main/java/com/example/mcnutt/inclassdemo/FragmentForData.java
@@ -22,22 +22,13 @@ public class FragmentForData extends Fragment {
         btnAdd = view.findViewById(R.id.btnAdd);
         textViewResult = view.findViewById(R.id.textViewResult);
 
-        Bundle bundle = getArguments();
-        final int firstNum = bundle.getInt(Constants.KEY_FIRST_NUMBER, 0);
-        final int secondNum = bundle.getInt(Constants.KEY_SECOND_NUMBER, 0);
-
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addTwoNumbers(firstNum, secondNum);
+                // Handling onClick from button
             }
         });
 
         return view;
-    }
-
-    private void addTwoNumbers(int firstNum, int secondNum) {
-        int result = firstNum + secondNum;
-        textViewResult.setText(String.format(getString(R.string.result_prompt), result));
     }
 }
