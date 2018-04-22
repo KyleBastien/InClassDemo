d47c9d6c0724bee1ee6714dd1bb6593d8d6ce33d
Kyle McNutt <mcnuttkyle93@gmail.com>
2018-04-22 15:26:25 -0700

    Setting up for Inter Fragment Communication Example

---
+++ app/src/main/java/com/example/mcnutt/inclassdemo/FragmentResult.java
@@ -0,0 +1,30 @@
+package com.example.mcnutt.inclassdemo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentResult extends Fragment {
+
+    private static final String TAG = FragmentResult.class.getSimpleName();
+
+    private TextView textViewResult;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_result, container, false);
+
+        textViewResult = view.findViewById(R.id.textResult);
+
+        return view;
+    }
+
+    public void addTwoNumbers(int num1, int num2) {
+
+    }
+}
