acbfaf08be5067b131c2c5ca420ce094e9d62aee
KyleMcNutt <mcnuttkyle93@gmail.com>
2018-04-22 15:25:29 -0700

    Revert "Adding Fragment Add/Remove Transaction Examples"
    This reverts commit c864fa9f63989d83bb45990328e2064f4c8ecd62.

--- app/src/main/java/com/example/mcnutt/inclassdemo/FragmentTransactionExampleActivity.java
+++ app/src/main/java/com/example/mcnutt/inclassdemo/FragmentTransactionExampleActivity.java
@@ -1,52 +1,26 @@
 package com.example.mcnutt.inclassdemo;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class FragmentTransactionExampleActivity extends AppCompatActivity {
 
-    private FragmentManager manager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_transaction_example);
-        manager = getFragmentManager();
     }
 
     public void addFragmentA(View view) {
-        Fragment fragmentA = new FragmentA();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.container, fragmentA, "fragA");
-        transaction.commit();
     }
 
     public void removeFragmentA(View view) {
-        FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
-        if (fragmentA != null) {
-            FragmentTransaction transaction = manager.beginTransaction();
-            transaction.remove(fragmentA);
-            transaction.commit();
-        }
     }
 
     public void addFragmentB(View view) {
-        Fragment fragmentB = new FragmentB();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.container, fragmentB, "fragB");
-        transaction.commit();
     }
 
     public void removeFragmentB(View view) {
-        FragmentB fragmentB = (FragmentB) manager.findFragmentByTag("fragB");
-        if (fragmentB != null) {
-            FragmentTransaction transaction = manager.beginTransaction();
-            transaction.remove(fragmentB);
-            transaction.commit();
-        }
     }
 }
