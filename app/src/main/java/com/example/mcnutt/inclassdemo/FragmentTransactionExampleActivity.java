2168d6ce646ccdd0705f4597378fc5ccca168670
Kyle McNutt <mcnuttkyle93@gmail.com>
2018-04-22 20:58:16 -0700

    Setting up for BackStack Example

--- app/src/main/java/com/example/mcnutt/inclassdemo/FragmentTransactionExampleActivity.java
+++ app/src/main/java/com/example/mcnutt/inclassdemo/FragmentTransactionExampleActivity.java
@@ -5,9 +5,12 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-public class FragmentTransactionExampleActivity extends AppCompatActivity {
+public class FragmentTransactionExampleActivity extends AppCompatActivity implements FragmentManager.OnBackStackChangedListener {
+
+    private static final String TAG = FragmentTransactionExampleActivity.class.getSimpleName();
 
     private FragmentManager manager;
 
@@ -16,12 +19,14 @@ public class FragmentTransactionExampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_transaction_example);
         manager = getFragmentManager();
+        manager.addOnBackStackChangedListener(this);
     }
 
     public void addFragmentA(View view) {
         Fragment fragmentA = new FragmentA();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.container, fragmentA, "fragA");
+        transaction.addToBackStack("AddFragA");
         transaction.commit();
     }
 
@@ -30,6 +35,7 @@ public class FragmentTransactionExampleActivity extends AppCompatActivity {
         if (fragmentA != null) {
             FragmentTransaction transaction = manager.beginTransaction();
             transaction.remove(fragmentA);
+            transaction.addToBackStack("RemoveFragA");
             transaction.commit();
         }
     }
@@ -38,6 +44,7 @@ public class FragmentTransactionExampleActivity extends AppCompatActivity {
         Fragment fragmentB = new FragmentB();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.container, fragmentB, "fragB");
+        transaction.addToBackStack("AddFragB");
         transaction.commit();
     }
 
@@ -46,6 +53,7 @@ public class FragmentTransactionExampleActivity extends AppCompatActivity {
         if (fragmentB != null) {
             FragmentTransaction transaction = manager.beginTransaction();
             transaction.remove(fragmentB);
+            transaction.addToBackStack("RemoveFragB");
             transaction.commit();
         }
     }
@@ -54,6 +62,7 @@ public class FragmentTransactionExampleActivity extends AppCompatActivity {
         FragmentA fragmentA = new FragmentA();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(R.id.container, fragmentA, "fragA");
+        transaction.addToBackStack("ReplaceByFragA");
         transaction.commit();
     }
 
@@ -61,6 +70,7 @@ public class FragmentTransactionExampleActivity extends AppCompatActivity {
         FragmentB fragmentB = new FragmentB();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(R.id.container, fragmentB, "fragB");
+        transaction.addToBackStack("ReplaceByFragB");
         transaction.commit();
     }
 
@@ -69,6 +79,7 @@ public class FragmentTransactionExampleActivity extends AppCompatActivity {
         if (fragmentA != null) {
             FragmentTransaction transaction = manager.beginTransaction();
             transaction.attach(fragmentA);
+            transaction.addToBackStack("AttachFragA");
             transaction.commit();
         }
     }
@@ -78,6 +89,7 @@ public class FragmentTransactionExampleActivity extends AppCompatActivity {
         if (fragmentA != null) {
             FragmentTransaction transaction = manager.beginTransaction();
             transaction.detach(fragmentA);
+            transaction.addToBackStack("DetachFragA");
             transaction.commit();
         }
     }
@@ -87,16 +99,43 @@ public class FragmentTransactionExampleActivity extends AppCompatActivity {
         if (fragmentA != null) {
             FragmentTransaction transaction = manager.beginTransaction();
             transaction.show(fragmentA);
+            transaction.addToBackStack("ShowFragA");
             transaction.commit();
         }
     }
 
-    public void hideFragmentB(View view) {
+    public void hideFragmentA(View view) {
         FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
         if (fragmentA != null) {
             FragmentTransaction transaction = manager.beginTransaction();
             transaction.hide(fragmentA);
+            transaction.addToBackStack("HideFragA");
             transaction.commit();
         }
+    }
+
+    public void dummyBackButtonClick(View view) {
+        manager.popBackStack();
+    }
+
+    public void popAddFragAInclusiveTransaction(View view) {
+        manager.popBackStack("AddFragA", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+    }
+
+    public void popAddFragBTransaction(View view) {
+        manager.popBackStack("AddFragB", 0);
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        int length = manager.getBackStackEntryCount();
+        StringBuilder msg = new StringBuilder();
+        for(int i = length - 1; i >= 0; i--) {
+            msg.append("Index ").append(i).append(" : ");
+            msg.append(manager.getBackStackEntryAt(i).getName());
+            msg.append("\n");
+        }
+
+        Log.i(TAG, "\n" + msg.toString() + " \n ");
     }
 }
