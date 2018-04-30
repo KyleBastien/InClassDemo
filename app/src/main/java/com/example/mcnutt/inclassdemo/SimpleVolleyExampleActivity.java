70e4dd0942f75158d8666cbfc0a234f0b0787cb7
Kyle McNutt <mcnuttkyle93@gmail.com>
2018-04-29 22:29:39 -0700

    Adding Simple Volley Request Example.

---
+++ app/src/main/java/com/example/mcnutt/inclassdemo/SimpleVolleyExampleActivity.java
@@ -0,0 +1,27 @@
+package com.example.mcnutt.inclassdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.mcnutt.inclassdemo.datamodels.LoremIpsumModel;
+import com.example.mcnutt.inclassdemo.viewmodels.LoremIpsumViewModel;
+
+public class SimpleVolleyExampleActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private LoremIpsumViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_volley_example);
+
+        LoremIpsumModel dataModel = new LoremIpsumModel(this);
+        viewModel = new LoremIpsumViewModel(dataModel);
+
+        textView = findViewById(R.id.textViewSimpleVolleyExample);
+
+        viewModel.getLoremIpsum(text -> textView.setText(text));
+    }
+}
---
+++ app/src/main/java/com/example/mcnutt/inclassdemo/datamodels/LoremIpsumModel.java
@@ -0,0 +1,25 @@
+package com.example.mcnutt.inclassdemo.datamodels;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class LoremIpsumModel {
+
+    private final String loremIpsumBaseApi = "https://loripsum.net/api/plaintext";
+    private RequestQueue queue;
+
+    public LoremIpsumModel(Context context) {
+        queue = Volley.newRequestQueue(context);
+    }
+
+    public StringRequest getLoremIpsum(Response.Listener<String> listener, Response.ErrorListener errorListener) {
+        StringRequest request = new StringRequest(Request.Method.GET, loremIpsumBaseApi, listener, errorListener);
+        queue.add(request);
+        return request;
+    }
+}
