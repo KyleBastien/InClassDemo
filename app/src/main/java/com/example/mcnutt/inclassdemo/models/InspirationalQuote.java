87130f5baf7c5e8536bf1e4a70c4c9e85ae19eeb
Kyle Bastien <kbastien@microsoft.com>
2019-10-03 10:49:47 -0700

    Trying to fix compile issue.

--- app/src/main/java/com/example/mcnutt/inclassdemo/models/InspirationalQuote.java
+++ app/src/main/java/com/example/mcnutt/inclassdemo/models/InspirationalQuote.java
@@ -1,29 +1,5 @@
 package com.example.mcnutt.inclassdemo.models;
 
-public class InspirationalQuoteTitle {
-  private String rendered;
-
-  public String getRendered() {
-    return rendered;
-  }
-
-  public void setRendered(String rendered) {
-    this.rendered = rendered;
-  }
-}
-
-public class InspirationalQuoteContent {
-  private String rendered;
-
-  public String getRendered() {
-    return rendered;
-  }
-
-  public void setRendered(String rendered) {
-    this.rendered = rendered;
-  }
-}
-
 public class InspirationalQuote {
     private String ID;
     private InspirationalQuoteTitle title;
