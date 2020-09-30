808eeec245431aef5463ddb8ea8b6304551962c0
Kyle Bastien <kbastien@microsoft.com>
2020-09-29 20:47:23 -0700

    Fixing mistake I accidently pushed

--- app/src/main/java/com/example/mcnutt/inclassdemo/SimpleFirebaseExample.java
+++ app/src/main/java/com/example/mcnutt/inclassdemo/SimpleFirebaseExample.java
@@ -9,7 +9,7 @@ import com.example.mcnutt.inclassdemo.viewmodels.FirebaseHelloWorldViewModel;
 
 public class SimpleFirebaseExample extends AppCompatActivity {
 
-    private FirebaseHelloWorldViewModel vmsssss;
+    private FirebaseHelloWorldViewModel vm;
     private TextView textView;
 
     @Override
