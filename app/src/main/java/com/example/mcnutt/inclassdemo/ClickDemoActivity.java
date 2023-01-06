ee95a17bd7b6526ad23a0a59bcb3667e4fa04bdd
Kyle Bastien <mcnuttkyle93@gmail.com>
2023-01-05 19:34:45 -0800

    I added a comment (#39)

--- app/src/main/java/com/example/mcnutt/inclassdemo/ClickDemoActivity.java
+++ app/src/main/java/com/example/mcnutt/inclassdemo/ClickDemoActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+// This is a comment, look how important this activity is
 public class ClickDemoActivity extends AppCompatActivity {
 
     private static final String TAG = ClickDemoActivity.class.getSimpleName();
